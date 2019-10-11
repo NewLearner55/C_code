417b10052d92d308dfe71901d48da7b5649ce921
C_code <564309357@qq.com>
2019-10-11 19:29:13 +0800

    打印100-200之间的素数

---
+++ 10_10/test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2; j < i;j++)
+		if (i%j == 0)
+		{
+			break;
+		}
+		if (i == j)
+		{
+			printf("%d\n", i);
+		}
+
+	}
+	return 0;
+}
