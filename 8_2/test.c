93f10a96a876076fb3527b50d56327b2603cb363
C_code <564309357@qq.com>
2019-11-05 11:46:20 +0800

    经典喝汽水

---
+++ 8_2/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int money = 0;
+	int total = 0;
+	int empty = 0;
+	scanf("%d", &money);
+
+	if (money>0)
+		total = money * 2 - 1;
+	else
+		total = 0;
+
+	/*total += money;
+	empty = money;
+	while(empty>=2)
+	{
+	total += empty/2;
+	empty = empty/2 + empty%2;
+	}*/
+
+	printf("%d\n", total);
+	return 0;
+}
